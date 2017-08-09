2475f55d3303e98ee558613baff462803019ce92
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-09 21:25:14 +0200

    fix tests

--- app/components/shopping-list.js
+++ app/components/shopping-list.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   remainingItems: Ember.computed('state.cartItems', 'state.shoppingListItems', function() {
+    if (!this.get('state')) { return [] }
     return this.get('state.shoppingListItems').reject((i) => {
       return this.get('state.cartItems').includes(i);
     });
