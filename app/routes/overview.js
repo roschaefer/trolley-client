2aacaddaaeccfcb20648943bd74e449166f4b741
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-09 19:10:46 +0200

    setup mirage

--- app/routes/overview.js
+++ app/routes/overview.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-    1,
-    2,
-    3,
-    4,
-  ];
+    return this.store.findAll('product');
   }
 });
