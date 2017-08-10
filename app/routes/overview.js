a1a16878efc1c8914c93141e69920ddc650091c6
Till Prochaska <mail@tillprochaska.de>
2017-08-11 00:54:59 +0200

    Unset button click event handlers in overview

--- app/routes/overview.js
+++ app/routes/overview.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  communication: Ember.inject.service(),
   sessionState: Ember.inject.service(),
   products: Ember.inject.service(),
   beforeModel(){
     this.get('sessionState').set('shoppingListItems', this.get('products').all());
+
+    // reset click on this button
+    this.get('communication').setEventListener('leftButtonClick', null);
+    this.get('communication').setEventListener('rightButtonClick', null);
   },
 });
