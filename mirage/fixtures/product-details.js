1bab5ea947048b5172b2ea7040b8f50538a18688
Kailyn Nguyen <kailyn@AdministorsMBP2.fritz.box>
2017-08-10 16:55:27 +0200

    regional details

--- mirage/fixtures/product-details.js
+++ mirage/fixtures/product-details.js
@@ -1,17 +1,281 @@
 export default [
-  {
-    productId: 4,
-    priority: "regional",
-    type: "slide",
-    filename: "schinken_basedahl_regional.jpg",
-    headline: "Echt regional",
-    text: "Schinkenhöker nennt sich Henning Basedahl. Er führt fort, was sein Vater vor über 40 Jahren in der Lüneburger Heide begonnen hat: Den besten Schinken weit und breit zu machen. Der Basedahler Schinken ist regional: Er kommt aus einer Manufaktur in Hollenstedt, 60 Kilometer von Lüneburg entfernt. Dort machen die 18 Mitarbeiter noch alles mit der Hand – und vor allem mit viel Zeit."
-  },{
-    productId: 4,
-    priority: 'organic',
-    type: "slide",
-    filename: "schinken_basedahl_bio.jpg",
-    headline: "Nix Bio - aus Überzeugung",
-    text: "Bei Basedahl betrachten sie Schinken als Handwerk mit Tradition. Für ihre Schinken verwenden sie bestes Fleisch, Salz und gute Späne zum Räuchern. Nicht mehr, nicht weniger, sagt Henning Basedahl, Chef der Schinkenmanufaktur – was solle da ein Bio-Siegel besser machen?"
-  }
-]
+    {
+        productId: 3,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Nicht gerade nebenan...',
+        text: 'Der Brokkoli kommt aus Parchim, dem Gemüsegarten Mecklenburg-Vorpommerns. Und ist ganz schön weit nach Niedersachsen gereist.',
+        filename: 'http://gph.is/1Cz4G2j'
+    },
+    {
+        productId: 4,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Nicht gerade nebenan...',
+        text: 'Der Bio-Brokkoli kommt aus Friedrichsgabekoog und hat damit auf dem Weg nach Lüneburg noch mehr Kilometer als der konventionelle Brokkoli hinter sich gelassen.',
+        filename: ''
+    },
+    {
+        productId: 5,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Eier aus der Nachbarschaft',
+        text: 'Der Geflügelhof Hirsch aus Handorf ist nur 17km von Lüneburg entfernt, die Eier haben es also nicht weit bis in den Markt.',
+        filename: ''
+    },
+    {
+        productId: 6,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Eier aus der Nachbarschaft',
+        text: 'Spitzenreiter in der Kategorie "Regionalität" - die Freilandeier vom Hof Müller-Hartmann brauchen bloß 13km bis in den Markt - quasi nebenan.',
+        filename: 'https://giphy.com/gifs/chicken-BIo5QXYD7LGbm'
+    },
+    {
+        productId: 7,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Weit gereist...',
+        text: 'Bio - aber aus der direkten Umgebung kommt er nicht. Stolze 112km brauchen die Eier vom Bauckhof bis in den Markt.',
+        filename: 'VIDEO Regional'
+    },
+    {
+        productId: 8,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Weit gereist...',
+        text: 'Wahrscheinlich ist dieser Lach Economy gereist – tiefgefroren, 7800 Kilometer bis in Deinen Einkaufswagen.',
+        filename: ''
+    },
+    {
+        productId: 9,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Regional – für Norweger',
+        text: 'Das Bio-Lachsfilet von followfish stammt auch nicht unbedingt aus der direkten Umgebung. Aber es kommt aus einer Zucht. Und hat so auf dem Weg von Norwegen bis in den Lüneburger Markt hat er immerhin "nur" 1437km hinter sich gelassen - ein Fünftel der Strecke, die das Lachsfilet von gut & günstig braucht.',
+        filename: 'http://gph.is/1sFG8oq'
+    },
+    {
+        productId: 10,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Regionaler als der Rest',
+        text: 'Mit einer Entfernung von 347km ist der junge Gouda von gut & günstig noch der Regionalisten Käse in dieser Kategorie.',
+        filename: ''
+    },
+    {
+        productId: 11,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Aufschneider?',
+        text: 'Die Molkerei Rücker aus dem ostfriesischem Aurich wirbt mit den Küsten Norddeutschlands. Die Milch wird regional auf 850 Höfen gemolken und zu Käse verarbeitet. Danach reist der Käse allerdings über 700 Kilometer weit nach Bayern und wieder zurück: zum Aufschneiden. Alter Schwede!',
+        filename: 'kaese_ruecker_regional.jpg'
+    },
+    {
+        productId: 12,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Schlägt die anderen',
+        text: 'Näher wird keiner produziert - nur 93km braucht die Schlagsahne von gut & günstig bi in den Markt nach Lüneburg.',
+        filename: ''
+    },
+    {
+        productId: 13,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'So geht regional!',
+        text: 'Unsere Karte zeigt das Milcheinzugsgebiet der Andechser Molkerei Scheitz. Die grünen Punkte markieren die Bio-Kuhbauern, die blauen Punkte markieren die Bio-Ziegenbauern.',
+        filename: 'sahne_andechser_regional.jpg'
+    },
+    {
+        productId: 14,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Mitelfeld in Sachen Regionalität',
+        text: '154km - so weit braucht die Hansano Schlagsahne von der Produktionsstätte in Upahl bis in den Lüneburger Markt. Immerhin Platz 2 in der Kategorie "Regionalität".',
+        filename: ''
+    },
+    {
+        productId: 15,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Herkunft? Unbekannt!',
+        text: 'Edeka sagt (noch) nicht, wo die Erdbeeren wachsen. Angegeben ist nur die 61 Kilometer entfernte Edeka Zentrale. Ob sie dort angebaut werden?',
+        filename: ''
+    },
+    {
+        productId: 16,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Nicht abgestrampelt',
+        text: 'Gerade einmal 18 Kilometer haben diese Erdbeeren bis in Deinen Einkaufswagen im Schnitt zurückgelegt. Die Felder des Spargelhofs Strampe liegen alle in der Nähe der Stadt.',
+        filename: 'erdbeeren_strampe_regional.jpg'
+    },
+    {
+        productId: 17,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Fisch von Welt',
+        text: 'Die Fische für diese Iglu-Fischstäbchen schwammen bis Februar 2017 im Nordwestpazifik und Nordostpazifik. Bis sie in Bremerhaven in Stäbchenform gepresst und paniert werden, haben sie 15.000 Kilometer zurückgelegt – gefroren. Ein halbes Jahr später liegen sie jetzt bei Dir im Einkaufswagen.',
+        filename: 'fischstaebchen_iglo_regional.jpg'
+    },
+    {
+        productId: 18,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Fisch von Welt',
+        text: 'Die Fische für diese Frosta Fischstäbchen schwammen bis Mai Februar 2017 im Nordwestpazifik und Nordostpazifik. Bis sie in Bremerhaven in Stäbchenform gepresst und paniert werden, haben sie 15.000 Kilometer zurückgelegt – gefroren. Ein halbes Jahr später liegen sie jetzt bei Dir im Einkaufswagen.',
+        filename: 'fischstaebchen_frosta_regional.jpg'
+    },
+    {
+        productId: 19,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Weltenbummler',
+        text: 'Beckings Sonntagskaffee wird in Hamburg von Hand geröstet. Die Rösterei ist nur 63 Kilometer entfernt. Die sechs verschiedenen Arabica-Kaffees wachsen aber in Papua Neuguinea, in Mittel-und Südamerika. Allein die Bohnen aus Nicaragua legen über 9000 Kilometer zurück – immerhin mit dem Schiff.',
+        filename: 'kaffee_becking_regional.jpg'
+    },
+    {
+        productId: 20,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Weltenbummler',
+        text: 'Wird vermutlich Hamburg geröstet, die Bohnen wachsen aber vermutlich in Vietnam und Brasilien. Wir haben die Bohnen aus Vietnam berechnet, die rund 31.000 Kilometer weit um die halbe Welt reisen.',
+        filename: 'kaffee_gug_regional.jpg'
+    },
+    {
+        productId: 21,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Keiner von hier',
+        text: 'Die Tempelmann Kaffeerösterei in Dorsten ist 353 Kilometer entfernt, die Hochland-Arabica-Bohnen kommen aus Honduras und Peru und sind rund 10.000km weit gereist.',
+        filename: 'kaffee_tempelmann_regional.jpg'
+    },
+    {
+        productId: 22,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Zimmerpflanze',
+        text: 'Edeka sagt (noch) nicht, wo der Spinat wächst. Angegeben ist nur die 61 Kilometer entfernte Edeka Zentrale. Ob er dort wächst?',
+        filename: 'tkspinat_edeka_regional.jpg'
+    },
+    {
+        productId: 23,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Blubb aus Borken',
+        text: 'Dieser Spinat stammt vom Iglo-Vertragslandwirt Lefting aus Borken-Marbeck. Er ist tiefgefroren mindestens 354 Kilometer gefahren.',
+        filename: 'tkspinat_iglo_regional.jpg'
+    },
+    {
+        productId: 24,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Kuckuck da unten',
+        text: 'Die Räucherei liegt 766 Kilometer entfernt, ist also ganz und gar nicht regional. Immerhin ist garantiert, dass der Schwarzwälder Schinken wenigstens eine Zeit im Schwarzwald im Rauch hing.',
+        filename: ''
+    },
+    {
+        productId: 25,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Echt Regional',
+        text: 'Schinkenhöker nennt sich Henning Basedahl. Er führt fort, was sein Vater vor über 40 Jahren in der Lüneburger Heide begonnen hat: Den besten Schinken weit und breit zu machen.  Der Basedahler Schinken ist regional: Er kommt aus einer Manufaktur in Hollenstedt, 60 Kilometer von Lüneburg entfernt. Dort machen die 18 Mitarbeiter noch alles mit der Hand – und vor allem mit  viel Zeit.',
+        filename: 'schinken_basedahl_regional.jpg'
+    },
+    {
+        productId: 26,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Heide ist das nicht.',
+        text: 'Der Schwarzwälder Schinken vor Ihnen wurde von hier aus mindestens 766 KIlometer nach Lüneburg gefahren. Ob man den Schwarzwald noch schmeckt?',
+        filename: 'schinken_gug_regional.jpg'
+    },
+    {
+        productId: 27,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Stolzer zweiter Platz',
+        text: 'Gut & günstig - und fast von nebenan. Mit 87km braucht die frische Vollmilch nur wenige Minuten länger für den Weg in den Markt, als die Milch von Alnatura',
+        filename: ''
+    },
+    {
+        productId: 28,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'The Milch next door...',
+        text: 'Die Milch von Alnatura - nicht nur durch verschiedene Siegel ausgezeichnet, sondern mit einem Weg von 78km auch noch das regionalste Produkt.',
+        filename: ''
+    },
+    {
+        productId: 29,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Milch aus Witzwort - kein Witz',
+        text: 'Gibt Orte, die glaubt man nicht. Aus Witzwort kommt diese Milch, das ist 189 Kilometer von Lüneburg entfernt.',
+        filename: ''
+    },
+    {
+        productId: 30,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Kühe von norddeutschen Wiesen',
+        text: '"Aus Norddeutschland - traditionell hergestellt" steht auf der Packung. Und tatsächlich braucht die Milch auch nur etwa 138km aus dem norddeutschen Lilienthal bis in den Markt.',
+        filename: ''
+    },
+    {
+        productId: 31,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Straight outta Viby',
+        text: 'Aus dem Arla-Werk unserer dänischen Freunde stammt dieser gut & günstig Frischkäse. Auf dem Weg von Viby (bei Aarhus) bis in den Lüneburger Markt legt er eine Strecke von 392km zurück.',
+        filename: ''
+    },
+    {
+        productId: 32,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Mia san Frischkäse',
+        text: 'Dieser Frischkäse stammt aus Regensburg. Zwar bio, aber regional ist anders.',
+        filename: ''
+    },
+    {
+        productId: 33,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Frischkäse aus Österreich',
+        text: 'Schlusslicht in der Kategorie Regionalität: Stolze 860km legt der Alnatura Frischkäse auf dem Weg von Österreich in den Lüneburger Markt zurück.',
+        filename: ''
+    },
+    {
+        productId: 34,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Multi-Kulti friert',
+        text: 'Die Herkunft der einzelnen Zutaten weist Frosta vorbildlich aus. Sie kommen aus Ecuador, Deutschland, Polen, Spanien, Belgien, Italien, Argentinien und Brasilien. Die Hauptzutat Brokkoli reist aus Ecuador über 10000km um die Welt.',
+        filename: ''
+    },
+    {
+        productId: 35,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Gebräu aus Bremen?',
+        text: 'Die Flasche weist als Absender Inbev Deutschland in Bremen aus. Ob das Pils auch dort gebraut wurde? Keine Information.',
+        filename: ''
+    },
+    {
+        productId: 36,
+        priority: 'regional',
+        type: 'slide',
+        headline: 'Komm näher!',
+        text: 'Legte man 100000 Flaschen „Gestopfter Dachs“-Bier längs aneinander, dann stünde man schon vor der Brauerei in Dahlenburg. Keine halbe Stunde Fahrt, zwischen Lüneburg und Wendland, wird dieses ungewöhnliche Bier gebraut: Mit dem wiederentdeckten alten Verfahren des „Hopfenstopfens“ gibt Brauer Marc Brammer dem Bier einen besonders blumigen Geschmack.',
+        filename: 'bier_dachs_regional.jpg'
+    },
+    {
+        productId: 37,
+        priority: 'regional',
+        type: 'slide',
+        headline: '',
+        text: '',
+        filename: ''
+    }]
