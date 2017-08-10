820f419456246edca0b9932c82b6901aa235a6cb
Kailyn Nguyen <kailyn@AdministorsMBP2.fritz.box>
2017-08-10 23:58:40 +0200

    equals helper

--- app/controllers/products/details.js
+++ app/controllers/products/details.js
@@ -3,7 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   sessionState: Ember.inject.service(),
-
+  detail: Ember.computed(function(){
+    let detail = this.get('model').detail;
+    let product = this.get('model').product;
+    if (typeof product.details[detail] !== 'undefined') {
+        return product.details[detail];
+    } else {
+        return null;
+    }
+  }),
   nextPriorityLabel: Ember.computed('sessionState.priorities', function() {
     let labels = {
         regional: 'Regional?',
---
+++ app/helpers/equals.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Helper.helper(function([value1, value2]) {
+    return value1 === value2;
+});
