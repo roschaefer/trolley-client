5d810f39b95d7e26580687e6dea4eb978a720c6c
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-10 12:06:29 +0200

    redirect from root to overview route

--- app/routes/application.js
+++ app/routes/application.js
@@ -14,5 +14,8 @@ export default Ember.Route.extend({
       //this.transitionTo('products', eventParams.id);
       this.get('sessionState').updatePriority(eventParams.priority, eventParams.state);
     });
+  },
+  redirect: function () {
+    this.transitionTo('overview');
   }
 });
